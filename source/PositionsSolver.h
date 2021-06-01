502ae36f9435af8bab183eac8d4fcc366fd52f37
Henry Nguyen <henry2833@gmail.com>
2021-06-01 14:39:08 -0700

    added positionssolver

--- source/PositionsSolver.h
+++ source/PositionsSolver.h
@@ -1,9 +1,11 @@
 #ifndef GAME_POSITIONSSOLVER_H
 #define GAME_POSITIONSSOLVER_H
-
+#include "TopDocs.h"
 
 namespace PositionsSolver {
-
+    TopDocs rescore_with_positions(const TopDocs& td, std::ifstream& positions, std::ifstream& terms) {
+        
+    }
 };
 
 

133f44a3f53ba614cfd19958fbe318b73596079d
Henry Nguyen <henry2833@gmail.com>
2021-06-30 12:49:08 -0700

    allowed stdindexer to be used as a library function rather than just executable

--- source/StdinIndexer.cpp
+++ source/StdinIndexer.cpp
@@ -8,9 +8,9 @@
 #include "GeneralIndexer.h"
 #include "Constants.h"
 #include "FileListGenerator.h"
+#include "StdinIndexer.h"
 #include "IndexFileLocker.h"
 
-void queue_produce_file_contents_stdin(SyncedQueue &contents);
 
 int main() {
     initialize_directory_variables();
@@ -31,59 +31,5 @@ int main() {
 }
 
 
-void queue_produce_file_contents_stdin(SyncedQueue &contents) {
-    std::vector<SyncedQueue::value_type> thread_local_holder;
-    uint docid = 0;
-    while (true) {
-        std::string file, filename;
-        file.reserve(10000);
-        std::string word;
-        std::cin >> word;
 
-        if (word == "/endindexing") {
-            contents.push_multi(thread_local_holder.begin(), thread_local_holder.end());
-            thread_local_holder.clear();
-            break;
-        }
-
-        // Filename
-        if (word != "filename") {
-            std::cerr << ("Word: " + word + " wrong");
-            exit(-1);
-        }
-        std::cin >> word;
-
-        while (word != "/endfilename") {
-            filename.append(word);
-            std::cin >> word;
-        }
-
-        std::cin >> word;
-        if (word != "file") {
-            std::cerr << ("Word: " + word + " wrong");
-            exit(-1);
-        }
-        std::cin >> word;
-        while (word != "/endfile") {
-            file.append(word + " ");
-            std::cin >> word;
-        }
-
-
-        if(search_name_database(FileListGenerator::get_ndb(), filename.c_str())) {
-            continue;
-        }
-
-        log("Processing file: ", filename);
-        thread_local_holder.emplace_back(std::move(file), DocIDFilePair{docid++, filename});
-
-        if (thread_local_holder.size() >= 50) {
-            contents.push_multi(thread_local_holder.begin(), thread_local_holder.end());
-            thread_local_holder.clear();
-            std::cout << "Progress: " << docid - contents.size() << "\n";
-        }
-    }
-    contents.done_flag = true;
-    contents.cv.notify_all();
-}
 
--- source/StdinIndexer.h
+++ source/StdinIndexer.h
@@ -5,10 +5,64 @@
 #ifndef GAME_STDININDEXER_H
 #define GAME_STDININDEXER_H
 
+#include "SyncedQueue.h"
 
-class StdinIndexer {
+inline void queue_produce_file_contents_stdin(SyncedQueue &contents) {
+    std::vector<SyncedQueue::value_type> thread_local_holder;
+    uint docid = 0;
+    while (true) {
+        std::string file, filename;
+        file.reserve(10000);
+        std::string word;
+        std::cin >> word;
 
-};
+        if (word == "/endindexing") {
+            contents.push_multi(thread_local_holder.begin(), thread_local_holder.end());
+            thread_local_holder.clear();
+            break;
+        }
 
+        // Filename
+        if (word != "filename") {
+            throw std::runtime_error ("Word: " + word + " wrong");
+        }
+        std::cin >> word;
+
+        while (word != "/endfilename") {
+            filename.append(word);
+            std::cin >> word;
+        }
+
+        std::cin >> word;
+        if (word != "file") {
+            std::cerr << ("Word: " + word + " wrong");
+            exit(-1);
+        }
+        std::cin >> word;
+        while (word != "/endfile") {
+            file.append(word + " ");
+            std::cin >> word;
+        }
+
+        // Erase the trailing whitespace
+        file.erase(file.end() -1 );
+
+
+        if (search_name_database(FileListGenerator::get_ndb(), filename.c_str())) {
+            continue;
+        }
+
+        log("Processing file: ", filename);
+        thread_local_holder.emplace_back(std::move(file), DocIDFilePair{docid++, filename});
+
+        if (thread_local_holder.size() >= 50) {
+            contents.push_multi(thread_local_holder.begin(), thread_local_holder.end());
+            thread_local_holder.clear();
+            std::cout << "Progress: " << docid - contents.size() << "\r";
+        }
+    }
+    contents.done_flag = true;
+    contents.cv.notify_all();
+}
 
 #endif //GAME_STDININDEXER_H

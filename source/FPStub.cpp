dbee354cb5953e0f13b8cf6d5fcfc34fe8cb96ba
Henry Nguyen <henry2833@gmail.com>
2021-06-30 12:48:28 -0700

    fixed bug with FPStub reading the size twice.

--- source/FPStub.cpp
+++ source/FPStub.cpp
@@ -3,13 +3,9 @@
 #include "FPStub.h"
 #include <fstream>
 
-#include <algorithm>
-
-
 FPStub::FPStub(const fs::path& path)  {
     auto stream = std::ifstream(path, std::ios_base::binary);
     assert(stream);
-    Serializer::read_vnum(stream);
     auto fp = Serializer::read_filepairs(stream);
     for (auto &p : fp) {
         map.emplace(p.document_id, p.file_name);
